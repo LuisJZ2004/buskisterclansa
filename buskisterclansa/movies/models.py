d6f922b9cabd94ebac3e5253c394dc99d5cb60f4
beamerboy <luisjimenez10005@gmail.com>
2023-03-07 15:29:38 +0000

    Movie model created

--- buskisterclansa/movies/models.py
+++ buskisterclansa/movies/models.py
@@ -1,3 +1,18 @@
+# Django
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
 
-# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    slug = models.SlugField(max_length=50, editable=False, blank=False, null=False)
+    year = models.DateField(default=timezone.now().date(), blank=False, null=False)
+    synopsis = models.TextField(max_length=1000, blank=False, null=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def save(self, *args, **kwargs) -> None:
+        self.slug = slugify(self.name)
+
+        return super().save(*args, **kwargs)
