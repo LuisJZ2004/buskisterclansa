4d29afd6cc389c3b03c6c7a3c82a6d8db4b074de
beamerboy <luisjimenez10005@gmail.com>
2023-03-08 15:12:16 +0000

    many_to_many models for directors, created_by and cast for MovieStaff and Movie

--- buskisterclansa/movies/models.py
+++ buskisterclansa/movies/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
 
+# Movie
 class Movie(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
     slug = models.SlugField(max_length=50, editable=False, blank=False, null=False)
@@ -13,6 +14,10 @@ class Movie(models.Model):
 
     genres = models.ManyToManyField(to='Genre')
 
+    created_by = models.ManyToManyField(to="MovieStaff", through="CreatedBy", related_name="movie_created_by")
+    directors = models.ManyToManyField(to="MovieStaff", through="Director", related_name="movie_director")
+    casts = models.ManyToManyField(to="MovieStaff", through="Cast", related_name="movie_cast")
+
     def __str__(self) -> str:
         return self.name
 
@@ -20,7 +25,8 @@ class Movie(models.Model):
         self.slug = slugify(self.name)
 
         return super().save(*args, **kwargs)
-    
+
+# Genre  
 class Genre(models.Model):
     name = models.CharField(unique=True, max_length=50, blank=False, null=False)
     slug = models.SlugField(max_length=50, blank=False, default=None, editable=False)
@@ -33,7 +39,29 @@ class Genre(models.Model):
         self.slug = slugify(self.name)
 
         return super().save(*args, **kwargs)
+
+# MovieStaff
 class MovieStaff(models.Model):
     name = models.CharField(max_length=70, blank=False, null=False)
     bio = models.TextField(max_length=1000, blank=False, null=False)
     image = models.ImageField(blank=True, null=True)
+
+class CreatedBy(models.Model):
+    movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE)
+    movie_staff = models.ForeignKey(to=MovieStaff, on_delete=models.CASCADE)
+
+    order = models.IntegerField(unique=True, default=0, blank=False, null=False)
+
+class Cast(models.Model):
+    movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE)
+    movie_staff = models.ForeignKey(to=MovieStaff, on_delete=models.CASCADE)
+
+    order = models.IntegerField(unique=True, default=0, blank=False, null=False)
+
+class Director(models.Model):
+    movie = models.ForeignKey(to=Movie, on_delete=models.CASCADE)
+    movie_staff = models.ForeignKey(to=MovieStaff, on_delete=models.CASCADE)
+
+    order = models.IntegerField(unique=True, default=0, blank=False, null=False)
+
+# Company
