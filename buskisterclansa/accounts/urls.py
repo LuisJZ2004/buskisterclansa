93bb5db24351df82ac05650adfcb207a423612b3
beamerboy <luisjimenez10005@gmail.com>
2023-03-13 10:03:06 +0000

    login and signin views and urls

--- buskisterclansa/accounts/urls.py
+++ buskisterclansa/accounts/urls.py
@@ -1,6 +1,12 @@
+# Django
 from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+# This app
+from .views import SingInView, CustomLoginView
 
 app_name="accounts"
 urlpatterns = [
-    
+    path("login/", CustomLoginView.as_view(template_name="accounts/register.html"), name="login_path"),
+    path("sign-in/", SingInView.as_view(), name="sign_in_path"),
 ]
--- buskisterclansa/accounts/views.py
+++ buskisterclansa/accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.views.generic import View
 
-# Create your views here.
+# This app
+from .forms import CustomUserCreationForm
+
+class SingInView(View):
+    def post(request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to="accounts:login_path")
+        else:
+            context = {
+                'form': form,
+                'errors': dict(form.errors),
+            }
+    def get(request):
+        return render(
+            request=request,
+            template_name="accounts/sign_in.html",
+            context= {
+                'form': CustomUserCreationForm()
+            }
+        )
+
+class CustomLoginView(LoginView):
+    def form_invalid(self, form):
+        # In the default LoginView it doesn't return the errors if any, so I send them if the form is not valid
+        return self.render_to_response(self.get_context_data(form=form, errors=form.errors))
