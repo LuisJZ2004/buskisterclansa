ea94905fe0dc058eef9cb8c4420b98a386624b22
beamerboy <luisjimenez10005@gmail.com>
2023-03-16 16:12:17 +0000

    layout

--- buskisterclansa/movie_staff/urls.py
+++ buskisterclansa/movie_staff/urls.py
@@ -1,6 +1,10 @@
+# Django
 from django.urls import path
+
+# This app
+from .views import MovieStaffView
 
 app_name="movie_staff"
 urlpatterns = [
-    
+    path("<slug>/<pk>/", MovieStaffView.as_view(), name="movie_staff_path"),
 ]
--- buskisterclansa/movie_staff/views.py
+++ buskisterclansa/movie_staff/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# Django
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 
-# Create your views here.
+# This app
+from .models import MovieStaff
+
+class MovieStaffView(DetailView):
+    model = MovieStaff
+    context_object_name = "staff"
+
+    def get_queryset(self):
+        return self.model.objects.all()
+    
+    def get_object(self, *args, **kwargs):
+        return get_object_or_404(klass=self.get_queryset(), slug=self.kwargs.get("slug"), pk=self.kwargs.get("pk"))
