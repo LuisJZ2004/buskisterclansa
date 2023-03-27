d80279d0298dc1132869866670d0e162abe6cc49
beamerboy <luisjimenez10005@gmail.com>
2023-03-27 09:59:11 +0000

    is_staff method created

--- buskisterclansa/movie_staff/models.py
+++ buskisterclansa/movie_staff/models.py
@@ -1,5 +1,7 @@
+# Django
 from django.db import models
 from django.utils.text import slugify
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 
 class MovieStaff(models.Model):
     name = models.CharField(max_length=70, blank=False, null=False)
@@ -14,3 +16,16 @@ class MovieStaff(models.Model):
         self.slug = slugify(self.name)
 
         return super().save(*args, **kwargs)
+
+    def is_staff(self, job_model: models.Model):
+        """
+        Returns if this staff has one or more of the movie_staff jobs (director, script etc.). The staff model
+        must be sended to make the query (Director, Cast, Producer etc.)
+        """
+        try:
+            job_model.objects.get(movie_staff__pk=self.pk)
+            return True
+        except MultipleObjectsReturned:
+            return True
+        except ObjectDoesNotExist: 
+            return False
