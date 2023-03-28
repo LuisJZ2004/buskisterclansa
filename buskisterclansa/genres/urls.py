57a4a453ed159e355385835ce17d6f03b2056322
beamerboy <luisjimenez10005@gmail.com>
2023-03-28 14:52:40 +0000

    view for genres made

--- buskisterclansa/genres/urls.py
+++ buskisterclansa/genres/urls.py
@@ -1,6 +1,10 @@
+# Django
 from django.urls import path
+
+# This app
+from .views import GenreView
 
 app_name="genres"
 urlpatterns = [
-    
+    path("<slug>/", GenreView.as_view(), name="genre_path"),
 ]
--- buskisterclansa/genres/views.py
+++ buskisterclansa/genres/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# Django
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View, DetailView
 
-# Create your views here.
+# This app
+from .models import Genre
+
+class GenreView(DetailView):
+    model=Genre
+    template_name="genres/genre.html"
+    context_object_name="genre"
+
+    def get_queryset(self):
+        return self.model.objects.all()
+    def get_object(self):
+        return get_object_or_404(klass=self.get_queryset(), slug=self.kwargs.get("slug"))
