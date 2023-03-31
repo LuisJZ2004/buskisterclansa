e08e811da3a5f9219e9b543b42864bd7731e7d80
beamerboy <luisjimenez10005@gmail.com>
2023-03-31 09:56:04 +0000

    search view and template created

--- buskisterclansa/search/urls.py
+++ buskisterclansa/search/urls.py
@@ -2,8 +2,9 @@
 from django.urls import path
 
 # This app
+from .views import SearchView
 
 app_name="search"
 urlpatterns = [
-
+    path("", SearchView.as_view(), name="search_path"),
 ]
--- buskisterclansa/search/views.py
+++ buskisterclansa/search/views.py
@@ -1,3 +1,19 @@
+# Django
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+# My apps
+from movies.models import Movie
+from movie_staff.models import MovieStaff
+
+class SearchView(View):
+    template_name="search/search.html"
+    
+    def get(self, request, *args, **kwargs):
+        return render(
+            request=request,
+            template_name=self.template_name,
+            context={
+                
+            }
+        )
