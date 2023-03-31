c8279932040a530d7ac91ce151a3f291fe4d0e2f
beamerboy <luisjimenez10005@gmail.com>
2023-03-31 10:53:36 +0000

    search header

--- buskisterclansa/search/views.py
+++ buskisterclansa/search/views.py
@@ -1,5 +1,5 @@
 # Django
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
 # My apps
@@ -10,10 +10,21 @@ class SearchView(View):
     template_name="search/search.html"
     
     def get(self, request, *args, **kwargs):
+
+        if not request.GET.get("search-bar"):
+            return redirect(to="home:home_path")
+
+        search = request.GET.get("search-bar")
+
+        movies = Movie.objects.filter(name__icontains=search)
+        staff = MovieStaff.objects.filter(name__icontains=search)
+
         return render(
             request=request,
             template_name=self.template_name,
             context={
-                
+                "search": request.GET.get("search-bar"),
+                "movies": movies,
+                "staff": staff,
             }
         )
